5a308d8b207eac2a3c0f222e4cb6374e6d5098ee
Ruham Leal <ruhamxlpro@hotmail.com>
2022-11-08 14:49:47 -0300

    feat: adding functionality that when you play a song you pause all other songs

--- frontend/src/components/MusicCard.jsx
+++ frontend/src/components/MusicCard.jsx
@@ -1,12 +1,20 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import getAllAudios from '../helpers/getAllAudios.js';
 import FavButton from './FavButton.jsx';
 
 function MusicCard({ audioUrl, musicName, trackId }) {
+  const handlePlay = ({ target }) => {
+    const audios = getAllAudios();
+    audios.forEach((audio) => {
+      if (audio !== target) audio.pause();
+    });
+  };
+
   return (
     <section>
       <h3>{ musicName }</h3>
-      <audio src={audioUrl} controls>
+      <audio onPlay={handlePlay} src={audioUrl} controls>
         <track kind="captions" />
         O seu navegador não suporta o elemento
         {' '}
