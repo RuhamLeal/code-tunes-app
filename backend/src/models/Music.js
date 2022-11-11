327fb0300fc0e2a364063d3c3c3da06d7400b6e2
Ruham Leal <ruhamxlpro@hotmail.com>
2022-11-11 14:15:52 -0300

    refactor: refactoring Music Model to add more information from the music

--- backend/src/models/Music.js
+++ backend/src/models/Music.js
@@ -7,6 +7,9 @@ const musicShema = new mongoose.Schema(
     trackId: { type: Number, required: true },
     audioUrl: { type: String, required: true },
     musicName: { type: String, required: true },
+    artistName: { type: String, required: true },
+    albumName: { type: String, required: true },
+    albumId: { type: Number, required: true },
   },
   {
     versionKey: false,
